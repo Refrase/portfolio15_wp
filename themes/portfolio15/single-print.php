23d9b9a0c3fbc748af8b5ec07d21ff6aa9d65cda
Andreas Rasmussen <andreasrefras@gmail.com>
2015-10-03 20:58:23 +0200

    Created Print layout

--- themes/portfolio15/single-print.php
+++ themes/portfolio15/single-print.php
@@ -7,6 +7,16 @@
       <li><img src="<?php the_field( 'print_1' ); ?>"></li>
     </ul>
 
+    <ul class="piece__pic piece__pic--print piece__pic--print--brochure">
+      <li><p class="piece__splitter"><?php the_field( 'brochure_1_title' ); ?></p></li>
+      <li><img src="<?php the_field( 'brochure_1' ); ?>"></li>
+    </ul>
+
+    <ul class="piece__pic piece__pic--print piece__pic--print--badges">
+      <li><p class="piece__splitter"><?php the_field( 'badges_1_title' ); ?></p></li>
+      <li><img src="<?php the_field( 'badges_1' ); ?>"></li>
+    </ul>
+
   <?php include( 'piecemeta-bottom.php' ); ?>
 
 <?php get_footer(); ?>
