3bc05017eb7b70857a6a11cf1f07a8c8d77517f9
Andreas Rasmussen <andreasrefras@gmail.com>
2015-10-03 16:36:43 +0200

    Started Print template

--- themes/portfolio15/single-print.php
+++ themes/portfolio15/single-print.php
@@ -2,6 +2,11 @@
 
   <?php include( 'piecemeta-top.php' ); ?>
 
+		<ul class="piece__pic piece__pic--print">
+      <li><p class="piece__splitter"><?php the_field( 'print_1_title' ); ?></p></li>
+      <li><img src="<?php the_field( 'print_1' ); ?>"></li>
+    </ul>
+
   <?php include( 'piecemeta-bottom.php' ); ?>
 
 <?php get_footer(); ?>
